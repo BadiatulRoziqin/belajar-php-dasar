22f310b0b385e60af35d5f18ea57bcc37d19a8a4
BadiatulRoziqin <badiatulroziqin82@gmail.com>
2024-05-06 13:19:20 +0700

    belajar tipe data number

--- TipeDataNumber.php
+++ TipeDataNumber.php
@@ -1,5 +1,31 @@
-<?php 
+<?php
 
-$i = 2;
-var_dump($i);
-?>
+echo "Desimal : ";
+var_dump(1234);
+
+echo "Octal : ";
+var_dump(01234);
+
+echo "Hexadesimal : ";
+var_dump(0x1A);
+
+echo "Binary : ";
+var_dump(0b11111);
+
+echo "Underscore in number : ";
+var_dump(1_234_234_234);
+
+echo "Floating Point : ";
+var_dump(1.234);
+
+echo "Floating Point dengan E Notation : (1.7 x 1000) : ";
+var_dump(1.7e3);
+
+echo "Floating Point dengan E Notation minus : (1.7 x 0.001) : ";
+var_dump(1.7e-3);
+
+echo "Underscore di Floating Point : ";
+var_dump(1_123.123);
+
+echo "Integer Overflow : ";
+var_dump(9223372036854775808);
